785b2c6078f7fa867178bae6984afee302c3c47d
62050219 <81694892+62050219@users.noreply.github.com>
2021-11-06 23:14:27 +0700

    Create suprawee.py

---
+++ suprawee.py
@@ -0,0 +1,18 @@
+import rospy
+from std_msgs.msg import String
+
+def talker():
+    pub = rospy.Publisher('suprawee_topic', String, queue_size=10)
+    rospy.init_node('suprawee', anonymous=True)
+    rate = rospy.Rate(5) # 5hz
+    while not rospy.is_shutdown():
+        name_str = "My name is Suprawee %s" % rospy.get_time()
+        rospy.loginfo(name_str)
+        pub.publish(name_str)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
